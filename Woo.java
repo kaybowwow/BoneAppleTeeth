42fedaa87a56fa4c1c946705fba8e85612f72ec9
kaybowwow <kbao@stuy.edu>
2017-01-18 22:39:22 -0500

    modified eating algo (temp)

--- Woo.java
+++ Woo.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class Woo{
     //====================V A R I A B L E S====================
-    public final static int maxMiles = 100;
+    public final static int maxMiles = 200;
     
     private Player pat;
     private Member spouse;
@@ -13,6 +13,7 @@ public class Woo{
     
     private ArrayList<String> inventory = new ArrayList<String>();
 
+    private int numAlive;
     private int daysTraveled;
     private static int milesTraveled;
     private int month;
@@ -93,7 +94,7 @@ public class Woo{
 		System.out.print (s);
 	    }
 	    else {
-		s = "\n\tChild " + (4 - membersLeft) + ":";
+		s = "\n\tChild " + (4 - membersLeft) + ": ";
 		System.out.print (s);
 	    }
 	    
@@ -118,213 +119,218 @@ public class Woo{
 
 	
 
-	    System.out.println("\n Before you head on the road, would you like to buy anything?");
-	    System.out.println("\n"+ "\t1: Yes" + "\t2: No");
+	System.out.println("\n Before you head on the road, would you like to buy anything?");
+	System.out.println("\n"+ "\t1: Yes" + "\t2: No");
 
-	    try {
-		selection = Keyboard.readInt();
-
-		if (selection == 1) {
-		    shop();
-		}
-
-		System.out.println(inventory);
-		//System.out.println(pat.money); 
-	    }
-	    catch (Exception e) {}
-	}
-
-	public void shop() {
-	    int minusMoney = 0;
-	    int selection;
-	    int selection2 = 0;
-	    String item = "";
-	    String continueShopping = "yes"; 
-	
-	    while ( continueShopping.equals("yes") && (pat.money > 0) ) {
-		System.out.println("Welcome to the shop! Here is your current amount of available cash: " +
-				   pat.money +
-				   "\nWhat would you like to buy?");
-		System.out.println("\n 1. oxen: $20 each" +
-				   "\n 2. medicine (Cures all): $10 each" +
-				   "\n 3. food : $7 each" +
-				   "\n 4. heavy jacket: $15 each"
-				   );
-		System.out.println("\n Selection: ");
-
-		//try {
-		selection = Keyboard.readInt();
-	    
-		if (selection == 1) {
-		    item = "oxen"; 
-		    System.out.println("How many?");
-		    selection2 = Keyboard.readInt();
-		    minusMoney = 20 * selection2;
-		    System.out.println("Estimated cost: " + minusMoney);
-		    if (pat.money-minusMoney > 0) {
-			System.out.println("Remaining money: " + (pat.money-minusMoney));
-		    }
-		}
-
-		if (selection == 2) {
-		    item = "medicine"; 
-		    System.out.println("How many?");
-		    selection2 = Keyboard.readInt();
-		    minusMoney = 10 * selection2;
-		    System.out.println("Estimated cost: " + minusMoney);
-		    if (pat.money-minusMoney > 0) {
-			System.out.println("Remaining money: " + (pat.money-minusMoney));
-		    }
-		}
-
-		if (selection == 3) {
-		    item = "food"; 
-		    System.out.println("How much?");
-		    selection2 = Keyboard.readInt();
-		    minusMoney = 7 * selection2;
-		    System.out.println("Estimated cost: " + minusMoney);
-		    System.out.println("Remaining money: " + (pat.money-minusMoney)); 
-		}
-	    
-		if (selection == 4) {
-		    item = "clothing"; 
-		    System.out.println("How many?");
-		    selection2 = Keyboard.readInt();
-		    minusMoney = 15 * selection2;
-		    System.out.println("Estimated cost: " + minusMoney);
-		    if (pat.money-minusMoney > 0) {
-			System.out.println("Remaining money: " + (pat.money-minusMoney));
-		    } 
-		}
-
-		if (minusMoney > pat.money){
-		    System.out.println("You can't afford that.");
-		}
-		
-		else{
-		    //subtracting money
-		    pat.money -= minusMoney;
-		    //adding to inventory: 
-		    for (int i = selection2; i > 0; i--) {
-			addItem(item);
-		    }
-		}
-		System.out.println("Continue shopping? Type 'yes' or 'no'");
-
-		continueShopping = Keyboard.readString();
-
-		if (continueShopping == "no") {
-		    for (int x = 0; x < inventory.size(); x++) {
-			inventory.add(inventory.get(x));
-		    }
-		}
-	    }
-	
-	    System.out.println("Items in inventory: " + inventory);
-	    System.out.println("Amount of money left: " + pat.money);
-	}
-
-	public boolean playTurn() {
-	    int selection = 1;
-	  
-
-	    setWeather();
-	    System.out.println("Current Weather: " + weather);
-
-	    System.out.println("Days traveled: " + daysTraveled);
-
-	    System.out.println("Miles Traveled: " + milesTraveled);
-	    
-
-	    int probabilityOccuring = (int)(Math.random() * 100); 
-
-	    if (probabilityOccuring < 50) {
-		randomEvent();
-		
-		if (child1.hasDisease) {
-		    child1.health -= 10;
-		}
-		if (child2.hasDisease){
-		    child2.health -= 10;
-		}
-		if (child3.hasDisease) {
-		    child3.health -=10;
-		}
-		if (spouse.hasDisease) {
-		    spouse.health -= 10;
-		}
-		if (pat.hasDisease) {
-		    pat.health -= 10;
-		}
-	    }
-
-  
-	    
-	    System.out.println("\nWhat do you want to do?");
-	    System.out.println("\t1: Continue\n" +
-			       "\t2: Rest\n" +
-			       "\t3: Check Location\n" +
-			       "\t4: Check Inventory\n" +
-			       "\t5: Check Stats");
-	    
+	try {
 	    selection = Keyboard.readInt();
 
 	    if (selection == 1) {
-		milesTraveled += pat.pace * 10;
-		int rationCounter = pat.ration;
-		while (rationCounter > 0) {
-		    if (haveItem("food")) {
-			System.out.println("yo eat");
-			removeItem("food");
-			rationCounter -= 1;
-		    }
-		    else {
-			System.out.println("no de food!!!");
-			if (pat.isAlive()) {
-			    pat.health -= 10;
-			    child1.health -= 10;
-			    child2.health -= 10;
-			    child3.health -= 10;
-			    spouse.health -= 10; 
-			}
-			else {
-			    return false;
-			}
-			rationCounter -= 1;
-		    }
+		shop();
+	    }
+
+	    System.out.println(inventory);
+	    //System.out.println(pat.money); 
+	}
+	catch (Exception e) {}
+    }
+
+    public void shop() {
+	int minusMoney = 0;
+	int selection;
+	int selection2 = 0;
+	String item = "";
+	String continueShopping = "yes"; 
+	
+	while ( continueShopping.equals("yes") && (pat.money > 0) ) {
+	    System.out.println("Welcome to the shop! Here is your current amount of available cash: " +
+			       pat.money +
+			       "\nWhat would you like to buy?");
+	    System.out.println("\n 1. oxen: $20 each" +
+			       "\n 2. medicine (Cures all): $10 each" +
+			       "\n 3. food : $7 each" +
+			       "\n 4. heavy jacket: $15 each"
+			       );
+	    System.out.println("\n Selection: ");
+
+	    //try {
+	    selection = Keyboard.readInt();
+	    
+	    if (selection == 1) {
+		item = "oxen"; 
+		System.out.println("How many?");
+		selection2 = Keyboard.readInt();
+		minusMoney = 20 * selection2;
+		System.out.println("Estimated cost: " + minusMoney);
+		if (pat.money-minusMoney > 0) {
+		    System.out.println("Remaining money: " + (pat.money-minusMoney));
 		}
 	    }
 
 	    if (selection == 2) {
-		rest();
+		item = "medicine"; 
+		System.out.println("How many?");
+		selection2 = Keyboard.readInt();
+		minusMoney = 10 * selection2;
+		System.out.println("Estimated cost: " + minusMoney);
+		if (pat.money-minusMoney > 0) {
+		    System.out.println("Remaining money: " + (pat.money-minusMoney));
+		}
 	    }
 
 	    if (selection == 3) {
-		if (milesTraveled <= 20){
-		    System.out.println("You're near the start.");
-		}
-		else if (milesTraveled >= 80){
-		    System.out.println("You're near the end.");
-		}
-		else{
-		    System.out.println("You're in the middle of nowhere.");
-		}
+		item = "food"; 
+		System.out.println("How much?");
+		selection2 = Keyboard.readInt();
+		minusMoney = 7 * selection2;
+		System.out.println("Estimated cost: " + minusMoney);
+		System.out.println("Remaining money: " + (pat.money-minusMoney)); 
 	    }
-
+	    
 	    if (selection == 4) {
-		printInventory();
+		item = "clothing"; 
+		System.out.println("How many?");
+		selection2 = Keyboard.readInt();
+		minusMoney = 15 * selection2;
+		System.out.println("Estimated cost: " + minusMoney);
+		if (pat.money-minusMoney > 0) {
+		    System.out.println("Remaining money: " + (pat.money-minusMoney));
+		} 
 	    }
 
-	    if (selection == 5) {
-		System.out.println(pat.about());
-		System.out.println(child1.about()); 
-		System.out.println(child2.about());
-		System.out.println(child3.about());
-		System.out.println(spouse.about());
+	    if (minusMoney > pat.money){
+		System.out.println("You can't afford that.");
 	    }
-	    daysTraveled += 1; 
-	    return true;
+		
+	    else{
+		//subtracting money
+		pat.money -= minusMoney;
+		//adding to inventory: 
+		for (int i = selection2; i > 0; i--) {
+		    addItem(item);
+		}
+	    }
+	    System.out.println("Continue shopping? Type 'yes' or 'no'");
+
+	    continueShopping = Keyboard.readString();
+
+	    if (continueShopping == "no") {
+		for (int x = 0; x < inventory.size(); x++) {
+		    inventory.add(inventory.get(x));
+		}
+	    }
 	}
+	
+	System.out.println("Items in inventory: " + inventory);
+	System.out.println("Amount of money left: " + pat.money);
+    }
+
+    public boolean playTurn() {
+	int selection = 1;
+
+	numAlive = 1;
+	if (spouse.isAlive()) {numAlive += 1;}
+	if (child1.isAlive()) {numAlive += 1;}
+	if (child2.isAlive()) {numAlive += 1;}
+	if (child3.isAlive()) {numAlive += 1;}
+	//System.out.println(numAlive);
+	
+
+	setWeather();
+	System.out.println("Current Weather: " + weather);
+
+	System.out.println("Days traveled: " + daysTraveled);
+
+	System.out.println("Miles Traveled: " + milesTraveled);
+	    
+
+	int probabilityOccuring = (int)(Math.random() * 100); 
+
+	if (probabilityOccuring < 50) {
+	    randomEvent();
+		
+	    if (child1.hasDisease) {
+		child1.health -= 10;
+	    }
+	    if (child2.hasDisease){
+		child2.health -= 10;
+	    }
+	    if (child3.hasDisease) {
+		child3.health -=10;
+	    }
+	    if (spouse.hasDisease) {
+		spouse.health -= 10;
+	    }
+	    if (pat.hasDisease) {
+		pat.health -= 10;
+	    }
+	}
+
+  
+	    
+	System.out.println("\nWhat do you want to do?");
+	System.out.println("\t1: Continue\n" +
+			   "\t2: Rest\n" +
+			   "\t3: Check Location\n" +
+			   "\t4: Check Inventory\n" +
+			   "\t5: Check Stats");
+	    
+	selection = Keyboard.readInt();
+
+	//we will revolutionize the consumption of food products !!!
+	if (selection == 1) {
+	    milesTraveled += pat.pace * 10;
+	    for (int x = 0 ; x < numAlive ; x += 1) {
+		if (haveItem("food")) {
+		    System.out.println("we got chipotle.");
+		    removeItem("food");
+		}
+		else {
+		    System.out.println("oh no potato famine");
+		    if (pat.isAlive()) {
+			if (x == 0) {pat.health -= 25;}
+			if (x == 1) {spouse.health -= 25;}
+			if (x == 2) {child1.health -= 25;}
+			if (x == 3) {child2.health -= 25;}
+			if (x == 4) {child3.health -= 25;}
+		    }
+		    else {
+			return false;
+		    }
+		}
+	    }
+	}
+
+	if (selection == 2) {
+	    rest();
+	}
+
+	if (selection == 3) {
+	    if (milesTraveled <= 20){
+		System.out.println("You're near the start.");
+	    }
+	    else if (milesTraveled >= 80){
+		System.out.println("You're near the end.");
+	    }
+	    else{
+		System.out.println("You're in the middle of nowhere.");
+	    }
+	}
+
+	if (selection == 4) {
+	    printInventory();
+	}
+
+	if (selection == 5) {
+	    System.out.println(pat.about());
+	    System.out.println(child1.about()); 
+	    System.out.println(child2.about());
+	    System.out.println(child3.about());
+	    System.out.println(spouse.about());
+	}
+	daysTraveled += 1; 
+	return true;
+    }
 	    
     public boolean rest(){
 	daysTraveled += 1;   
@@ -489,70 +495,70 @@ public class Woo{
 	
     
     
-	//^^^^^^^^^^i m p o r t a n t   t h i n g s^^^^^^^^^^
+    //^^^^^^^^^^i m p o r t a n t   t h i n g s^^^^^^^^^^
 
-	//==========h e l p e r   f u n c t i o n s==========
+    //==========h e l p e r   f u n c t i o n s==========
 		
-	public int getFirstIndexOf (String item) {
-	    if (haveItem(item)) {
-		for (int x = 0 ; x < inventory.size() ; x += 1) {
-		    if (inventory.get(x) == item) {
-			return x;
-		    }
-		}
-	    }
-	    return -1;
-	}
-
-	public void addItem (String item) {
-	    if (haveItem(item)) {
-		inventory.add(getFirstIndexOf(item) , item);
-	    }
-	    else {
-		inventory.add(item);
-	    }
-	}
-
-	public void removeItem(String item) {
-	    if (haveItem(item)) {
-		inventory.remove(getFirstIndexOf(item));
-	    }
-	}
-
-	public boolean haveItem (String item) {
+    public int getFirstIndexOf (String item) {
+	if (haveItem(item)) {
 	    for (int x = 0 ; x < inventory.size() ; x += 1) {
 		if (inventory.get(x) == item) {
-		    return true;
+		    return x;
 		}
 	    }
-	    return false;
 	}
+	return -1;
+    }
 
-	public int countItem (String item) {
-	    int quantity = 0;
-	    if (haveItem(item)) {
-		for (int x = 0 ; x < inventory.size() - 1 ; x += 1) {
-		    if (inventory.get(x) == item) {
-			quantity += 1;
-		    }
+    public void addItem (String item) {
+	if (haveItem(item)) {
+	    inventory.add(getFirstIndexOf(item) , item);
+	}
+	else {
+	    inventory.add(item);
+	}
+    }
+
+    public void removeItem(String item) {
+	if (haveItem(item)) {
+	    inventory.remove(getFirstIndexOf(item));
+	}
+    }
+
+    public boolean haveItem (String item) {
+	for (int x = 0 ; x < inventory.size() ; x += 1) {
+	    if (inventory.get(x) == item) {
+		return true;
+	    }
+	}
+	return false;
+    }
+
+    public int countItem (String item) {
+	int quantity = 0;
+	if (haveItem(item)) {
+	    for (int x = 0 ; x < inventory.size() - 1 ; x += 1) {
+		if (inventory.get(x) == item) {
+		    quantity += 1;
 		}
 	    }
-	    return quantity;
 	}
-	//^^^^^^^^^^h e l p e r f u n c t i o n s^^^^^^^^^^
+	return quantity;
+    }
+    //^^^^^^^^^^h e l p e r f u n c t i o n s^^^^^^^^^^
 
 	
     
-	public static void main (String[] args){
-	    Woo game = new Woo();
+    public static void main (String[] args){
+	Woo game = new Woo();
 
-	    milesTraveled = 0;
+	milesTraveled = 0;
 
-	    while (milesTraveled < maxMiles) {
-		if (!game.playTurn())
-		    break;
-		System.out.println();
-	    }
-	    System.out.println("Your game is over.");
+	while (milesTraveled < maxMiles) {
+	    if (!game.playTurn())
+		break;
+	    System.out.println();
 	}
+	System.out.println("Your game is over.");
     }
+}
